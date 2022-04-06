59855315abdf2c283a0b936dee4464a0280390b7
Alejandro Uribe Sánchez <alejouribesanchez@gmail.com>
2022-04-05 22:41:42 -0500

    Use environment variables to set credentials

--- LensReceiptsDemo/app/src/main/java/com/veryfi/lens/receipts/demo/Application.kt
+++ LensReceiptsDemo/app/src/main/java/com/veryfi/lens/receipts/demo/Application.kt
@@ -9,10 +9,10 @@ class Application: Application() {
 
     companion object {
         //REPLACE YOUR KEYS HERE
-        const val CLIENT_ID = "xxx"
-        const val AUTH_USERNAME = "xxx"
-        const val AUTH_API_KEY = "xxx"
-        const val URL = "https://xxx.xxx"
+        const val CLIENT_ID = BuildConfig.VERYFI_CLIENT_ID
+        const val AUTH_USERNAME = BuildConfig.VERYFI_USERNAME
+        const val AUTH_API_KEY = BuildConfig.VERYFI_API_KEY
+        const val URL = BuildConfig.VERYFI_URL
     }
 
     override fun onCreate() {
